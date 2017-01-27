3cfce7c76314244715c97c15627b292ee17f3223
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2017-01-28 00:23:08 +0700

    emit event from client and server

---
+++ Starter/app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var port = 8080;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on('connection', function(socket) {
+	console.log('New connection');
+	//send message from server
+	socket.emit('message-from-server', {
+		greeting: 'Hello from server'
+	});
+
+	//receive msg from client
+	socket.on('message-from-client', function(msg) {
+		console.log(msg);
+	});
+});
+
+server.listen(port, function() {
+	console.log("Listening on port " + port);
+});
